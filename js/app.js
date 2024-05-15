a2458a9e298486d4d23854a8d56e1f87357f426e
Ramankar Chauhan <ascars478@gmail.com>
2024-05-15 16:03:51 +0530

    Build7

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/6.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/7.json");
 let WebXR;
 window.ARWT = {}
 
